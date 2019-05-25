acb9dbf908b4b1d07846e9fe3c83f34acf61a19d
BelllTree <BelllTree@192.168.0.40>
2019-05-25 15:09:47 +0900

    ループとスレッドを取りやめ。IFTTTにアップロードするだけの関数にした。

--- temperature.py
+++ temperature.py
@@ -14,7 +14,7 @@ temperature = 0
 humidity = 0
 
 # Run TCP-IP Socket transfer
-def recSocketvData(command):
+def recSocketData(command):
     s = socket.socket()
 
     #with closing(s):
@@ -27,7 +27,7 @@ def recSocketvData(command):
 
 # Receive temprature data by TCP-IP Socket
 def getTempBySocket():
-    data = recSocketvData(b'getTemperature')
+    data = recSocketData(b'getTemperature')
     # set global
     global temperature
     temperature = data.split(',')[0]
@@ -37,44 +37,58 @@ def getTempBySocket():
     return data
 
 
+# Upload temperature data to IFTTT
+def uploadIFTTT():
+    getTempBySocket()
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    #data = '{"value1":"2018/01/06 0:03:03","value2":"24","value3":"33"}'
+    data = '{"value1":"' + str(now) + '","value2":"' + str(temperature) + '","value3":"' + str(humidity) + '"}'
+    response = requests.post('https://maker.ifttt.com/trigger/temperature/with/key/dZoUaA3eWWF2H0HmNNfDtb', headers=headers, data=data)
+    print(temperature + ' ' + humidity)
+
+
 def getTemperature():
     return temperature
+
 
 def getHumidity():
     return humidity
 
 
-def loop():
-    # get temperature at first
-    getTempBySocket()
-
-    while True:
-        now = datetime.datetime.now()
-        if ((now.minute % 30)  == 0):     # every 30 minutes
-            getTempBySocket()
-
-            # Send data to IFTTT
-            headers = {
-                'Content-Type': 'application/json',
-            }
-            #data = '{"value1":"2018/01/06 0:03:03","value2":"24","value3":"33"}'
-            data = '{"value1":"' + str(now) + '","value2":"' + str(temperature) + '","value3":"' + str(humidity) + '"}'
-            response = requests.post('https://maker.ifttt.com/trigger/temperature/with/key/dZoUaA3eWWF2H0HmNNfDtb', headers=headers, data=data)
-
-            print(temperature + ' ' + humidity)
-            #print(data)
-            time.sleep(60)
-
-        time.sleep(1)
-
-
-def startGetTempThread():
-    th = threading.Thread(target=loop)
-    th.start()
+# def loop():
+#     # get temperature at first
+#     getTempBySocket()
+#
+#     while True:
+#         now = datetime.datetime.now()
+#         if ((now.minute % 30)  == 0):     # every 30 minutes
+#             getTempBySocket()
+#
+#             # Send data to IFTTT
+#             headers = {
+#                 'Content-Type': 'application/json',
+#             }
+#             #data = '{"value1":"2018/01/06 0:03:03","value2":"24","value3":"33"}'
+#             data = '{"value1":"' + str(now) + '","value2":"' + str(temperature) + '","value3":"' + str(humidity) + '"}'
+#             response = requests.post('https://maker.ifttt.com/trigger/temperature/with/key/dZoUaA3eWWF2H0HmNNfDtb', headers=headers, data=data)
+#
+#             print(temperature + ' ' + humidity)
+#             #print(data)
+#             time.sleep(60)
+#
+#         time.sleep(1)
+#
+#
+# def startGetTempThread():
+#     th = threading.Thread(target=loop)
+#     th.start()
 
 
 # Call getTemperature() only when it is executed directry
 if __name__ == '__main__':
-    loop()
+    uploadIFTTT()
+    #loop()
 
 
